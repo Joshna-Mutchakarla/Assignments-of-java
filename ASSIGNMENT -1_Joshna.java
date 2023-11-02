724c7f815e2fd0523418cc307d1e6c7a947d8e31
Joshna-Mutchakarla <101091267+Joshna-Mutchakarla@users.noreply.github.com>
2023-11-02 14:21:52 +0530

    Add files via upload

---
+++ ASSIGNMENT -1_Joshna.java
@@ -0,0 +1,38 @@
+ASSIGNMENT -1:
+
+STUDENT PROGRAM:
+--------------------------------
+package com.Java.Assignments;
+
+public class Student {
+	private String studentName; //here we declared private 
+	private String collegeName;
+	private int studentID;
+	
+
+	public static void main(String[] args) {
+		System.out.println("Successful");
+	}
+
+}
+
+-----------------------------------------------------------------------------------------------------------------------------------
+EMPLOYEE PROGRAM:
+-----------------
+package com.Java.Assignments;
+
+public class Employee {
+	  protected int id; //here we declared protected
+	  protected int age =(int)35.5;//here we converting age  double to int
+	  protected String Employeename;
+	  boolean Permanent;
+
+
+	 public static void main(String[] args)
+	  {
+	     System.out.println("Successfully started");
+
+	}
+
+}
+
